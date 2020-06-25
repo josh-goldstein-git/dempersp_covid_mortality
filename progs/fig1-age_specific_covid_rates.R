25da450614d0bd778981dc0f45df8a3cf842545c
Casey Breen <caseyfbreen@gmail.com>
2020-06-25 16:31:25 -0700

    new .tex file

---
+++ progs/fig1-age_specific_covid_rates.R
@@ -0,0 +1,8 @@
+#########################################
+# Calculate Age-Specific Covid Rates
+#########################################
+
+# We plot the unnormalized and normalized age-specific death rates 
+# across regions. 
+
+
